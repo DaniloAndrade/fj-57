a06c1758ee9e8c843eb2578458edc691063c188a
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2015-09-12 16:50:31 -0300

    implementado helper para formulario

---
+++ Cadastro/app/src/main/java/br/com/caelum/cadastro/helper/FormularioHelper.java
@@ -0,0 +1,58 @@
+package br.com.caelum.cadastro.helper;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import br.com.caelum.cadastro.FormularioActivity;
+import br.com.caelum.cadastro.R;
+import br.com.caelum.cadastro.modelo.Aluno;
+
+/**
+ * Created by android5243 on 12/09/15.
+ */
+public class FormularioHelper {
+
+
+    private FormularioActivity formularioActivity;
+    private final Aluno aluno;
+    private final EditText email;
+    private final RatingBar nota;
+    private final EditText site;
+    private final EditText endereco;
+    private final EditText telefone;
+    private final EditText nome;
+
+    public FormularioHelper(FormularioActivity formularioActivity) {
+        this.formularioActivity = formularioActivity;
+        nome = (EditText) formularioActivity.findViewById(R.id.formulario_nome);
+        telefone = (EditText) formularioActivity.findViewById(R.id.formulario_telefone);
+        endereco = (EditText) formularioActivity.findViewById(R.id.formulario_endereco);
+        email = (EditText) formularioActivity.findViewById(R.id.formulario_email);
+
+        site = (EditText) formularioActivity.findViewById(R.id.formulario_site);
+        nota = (RatingBar) formularioActivity.findViewById(R.id.formulario_nota);
+        aluno = new Aluno();
+    }
+
+    public Aluno pegaAlunoDoFormulario(){
+        aluno.setNome(nome.getText().toString());
+        aluno.setTelefone(telefone.getText().toString());
+        aluno.setEndereco(endereco.getText().toString());
+        aluno.setEmail(email.getText().toString());
+        aluno.setSite(site.getText().toString());
+        aluno.setNota((double) nota.getRating());
+
+        return aluno;
+    }
+
+    public boolean estaValido() {
+        //Adicionar outras validações
+
+        return !nome.getText().toString().isEmpty();
+    }
+
+    public void mostrarErros() {
+        nome.setError("Campo nome não pode ser vazio");
+    }
+}
